48d44f3a69b44b8c159af6dc5758270fd5b5ca95
Brian Gastón di Gangi Pedersen <briandigangi@hotmail.com>
2022-09-13 15:04:58 +0200

    +Options(under develop) +improvements +bug fixes

--- Menu.xaml.cs
+++ Menu.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using SnakeProjekt.Models;
+using System.IO;
+using System.Xml.Serialization;
+using Microsoft.Win32;
 
 namespace SnakeProjekt
 {
@@ -20,34 +24,61 @@ namespace SnakeProjekt
     /// </summary>
     public partial class Menu : Window
     {
-        MainWindow main = new();
+        static HighScore score = new();
+        MainWindow main = new(score);
         public Menu()
         {
             this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            LoadHighscoreList();
+            this.DataContext = score;
         }
+        
         private void Start_Click(object sender, EventArgs e)
         {
-            this.Close();
-            main.ShowDialog();
+            main.Show();
         }
+
+        #region Score
         private void BtnShowHighscoreList_Click(object sender, RoutedEventArgs e)
         {
             StartBtn.Visibility = Visibility.Collapsed;
             bdrHighscoreList.Visibility = Visibility.Visible;
         }
+        private void LoadHighscoreList()
+        {
+            if (File.Exists("snake_highscorelist.xml"))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<SnakeHighScore>));
+                using (Stream reader = new FileStream("snake_highscorelist.xml", FileMode.Open))
+                {
+                    List<SnakeHighScore> tempList = (List<SnakeHighScore>)serializer.Deserialize(reader);
+                    score.HighscoreList.Clear();
+                    foreach (var item in tempList.OrderByDescending(x => x.Score))
+                        score.HighscoreList.Add(item);
+                }
+            }
+        }
+        #endregion
+
+        #region Buttons Interactions
         private void BtnClose_Click(object sender, RoutedEventArgs e)
         {
-            this.Close();
-            main.Close();
+            Application.Current.Shutdown();
         }
         private void BtnBack_Click(object sender, RoutedEventArgs e)
         {
-            
+            bdrHighscoreList.Visibility = Visibility.Collapsed;
+            StartBtn.Visibility = Visibility.Visible;
         }
-
         private void window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
         }
+        #endregion
+
+        private void OptionsWindow_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
     }
-}
+}   
